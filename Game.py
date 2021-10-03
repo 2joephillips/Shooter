d970f05c15f27801c2046f176b70914f30cbd745
Joseph Phillips <joseph@josephephillips.com>
2021-10-03 15:22:58 -0400

    add player class

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import pygame
+import soldier
 
 pygame.init()
 
@@ -8,13 +9,20 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
+player = soldier.Soldier(200,200,3)
+enemy = soldier.Soldier(400,400,3)
+
 run = True
 while run:
+
+  player.draw(screen)
+  enemy.draw(screen)
 
   for event in pygame.event.get():
     # quit game
     if event.type == pygame.QUIT:
       run = False
-
+  
+  pygame.display.update()
 
 pygame.quit()
